88270616a5892129847cfbb73623acfd1a23255c
Mike Gimelfarb <35513382+mike-gimelfarb@users.noreply.github.com>
2023-11-01 19:23:24 -0400

    Initial commit

---
+++ baselines/jaxplan.py
@@ -0,0 +1,51 @@
+from datetime import datetime
+import jax
+
+from pyRDDLGym.Core.Jax.JaxParameterTuning import JaxParameterTuningSLP
+from pyRDDLGym.Core.Jax.JaxParameterTuning import JaxParameterTuningSLPReplan
+from pyRDDLGym.Core.Jax.JaxRDDLBackpropPlanner import _load_config
+from pyRDDLGym.Core.Jax.JaxRDDLBackpropPlanner import JaxRDDLBackpropPlanner
+from pyRDDLGym.Core.Jax.JaxRDDLBackpropPlanner import JaxOfflineController
+from pyRDDLGym.Core.Jax.JaxRDDLBackpropPlanner import JaxOnlineController
+
+
+def jax_policy(env, online, do_tune,
+               config, args, planner_args, plan_args, train_args,
+               outputpath, global_args):
+    
+    # run hyper-parameter tuning
+    if do_tune:
+        if online:
+            tuning_obj = JaxParameterTuningSLPReplan 
+        else:
+            tuning_obj = JaxParameterTuningSLP
+        
+        # create tuning instance with horizon range from environment
+        tuning = tuning_obj(env=env,
+                            timeout_training=args['train_seconds'],
+                            planner_kwargs=planner_args,
+                            plan_kwargs=plan_args,
+                            num_workers=global_args['gp_cpus'],
+                            gp_iters=global_args['gp_iters_jax'])
+        tuning.hyperparams_dict['T'] = (1, env.horizon, int)
+        
+        params = tuning.tune(key=jax.random.PRNGKey(int(datetime.now().timestamp())),
+                             filename=outputpath + '_gp')
+                
+        # update config with optimal hyper-parameters
+        args['method_kwargs']['initializer'] = 'normal'
+        args['method_kwargs']['initializer_kwargs'] = {'stddev': params['std']}
+        args['optimizer_kwargs']['learning_rate'] = params['lr']
+        args['logic_kwargs']['weight'] = params['w']
+        args['policy_hyperparams'] = {k: params['wa'] for k in args['policy_hyperparams']}
+        if online:
+            args['rollout_horizon'] = params['T']       
+        planner_args, plan_args, train_args = _load_config(config, args)
+            
+    # solve planning problem with new optimal parameters
+    planner = JaxRDDLBackpropPlanner(rddl=env.model, **planner_args)    
+    if online:
+        return JaxOnlineController(planner, **train_args)
+    else:
+        return JaxOfflineController(planner, train_on_reset=True, **train_args)
+
---
+++ main.py
@@ -0,0 +1,81 @@
+import json
+import os
+import sys
+
+from pyRDDLGym.Core.Env.RDDLEnv import RDDLEnv
+from pyRDDLGym.Core.Jax.JaxRDDLBackpropPlanner import _parse_config_file, _load_config
+from pyRDDLGym.Core.Policies.Agents import NoOpAgent, RandomAgent
+
+from rddlrepository.Manager.RDDLRepoManager import RDDLRepoManager as RDDLRepoManager
+
+from baselines.gurobiplan import gurobi_policy
+from baselines.jaxplan import jax_policy
+
+# load global config
+ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
+_, global_args = _parse_config_file(os.path.join(ROOT_PATH, 'global.cfg'))
+
+
+def main(domain, instance, method, online, do_tune):
+    outputpath = os.path.join(ROOT_PATH, 'outputs', method, 
+                              f'{domain}_{instance}_{method}_{online}')
+    
+    # create the environment
+    EnvInfo = RDDLRepoManager().get_problem(domain)   
+    env = RDDLEnv(domain=EnvInfo.get_domain(),
+                  instance=EnvInfo.get_instance(instance),
+                  enforce_action_constraints=True,
+                  log=True, 
+                  log_path=outputpath)
+    
+    # load the config file with planner settings
+    config, args = _parse_config_file(os.path.join(ROOT_PATH, 'configs', f'{domain}.cfg'))
+    
+    # override default config settings here
+    args['train_seconds'] = global_args['train_seconds_per_epoch'] * (1 if online else env.horizon)
+    if not online:
+        args['rollout_horizon'] = None
+    planner_args, plan_args, train_args = _load_config(config, args)
+    
+    # dispatch to policy creation method
+    if method == 'jax':
+        policy = jax_policy(env, online, do_tune, 
+                            config, args, planner_args, plan_args, train_args, 
+                            outputpath, global_args)
+        ground_state = False
+        
+    elif method == 'gurobi':
+        policy = gurobi_policy(env, online, do_tune, args, outputpath, global_args)
+        ground_state = True
+    
+    elif method in ['noop', 'random']:
+        if method == 'noop':
+            policy = NoOpAgent(action_space=env.action_space,
+                               num_actions=env.numConcurrentActions)
+        else:
+            policy = RandomAgent(action_space=env.action_space,
+                                 num_actions=env.numConcurrentActions)
+        ground_state = True
+    
+    else:
+        raise Exception(f'Invalid method {method}.')
+    
+    # evaluation
+    result = policy.evaluate(env, 
+                             verbose=True, 
+                             episodes=global_args['runs'], 
+                             ground_state=ground_state)
+    
+    # dump all history to files
+    with open(outputpath + '.json', 'w') as fp:
+        json.dump(result, fp, indent=4)
+    env.close()
+
+
+if __name__ == '__main__':
+    domain, instance, method, online, do_tune = 'CooperativeRecon_MDP_ippc2011', '3', 'gurobi', True, True
+    # domain, instance, method, do_tune = sys.argv[1:5]
+    online = online in {'True', 'true', True, '1', 1}
+    do_tune = do_tune in {'True', 'true', True, '1', 1}
+    main(domain, instance, method, online, do_tune)
+    
