bedd5d85208e31407397c4dc3365338aa0ae457f
Mike Gimelfarb <35513382+mike-gimelfarb@users.noreply.github.com>
2023-11-06 00:41:29 -0500

    Fixed wrong file names saved in prost docker

--- baselines/prost.sh
+++ baselines/prost.sh
@@ -12,15 +12,13 @@ fi
 DOM="$1"
 INST="$2"
 
-SUFFIX = "_$1_$2_prost_True_$4"
-
 echo "Starting RDDL Gym Server..."
-( cd /workspace/pyRDDLGym && python prost.py "$DOM" "$INST" "$3" "$4" ) > pyrddl$SUFFIX.log 2>&1 &
+( cd /workspace/pyRDDLGym && python prost.py "$DOM" "$INST" "$3" "$4" ) > pyrddl_$1_$2_prost_True_$4.log 2>&1 &
 
 sleep 5
 echo "Starting PROST..."
 ( cd /workspace/prost && ./search-release 813rocks \
-"[PROST -s 1 -se [THTS -act [UCB1] -out [UMC] -backup [PB] -init [Expand -h [IDS]]]]" ) > prost$SUFFIX.log 2>&1
+"[PROST -s 1 -se [THTS -act [UCB1] -out [UMC] -backup [PB] -init [Expand -h [IDS]]]]" ) > prost_$1_$2_prost_True_$4.log 2>&1
 
 echo "Writing data.json..."
-cat /workspace/data.json > data$SUFFIX.json
+cat /workspace/data.json > data_$1_$2_prost_True_$4.json
