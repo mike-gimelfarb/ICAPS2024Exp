cae6f0b09afe5492855031a6c3fd97b4ce0eaf8d
Mike Gimelfarb <35513382+mike-gimelfarb@users.noreply.github.com>
2023-11-06 01:20:25 -0500

    Added correct time limit per step in prost.sh

--- baselines/prost.sh
+++ baselines/prost.sh
@@ -11,14 +11,15 @@ fi
 # File paths from rddlrepository
 DOM="$1"
 INST="$2"
+TIME=999999
 
 echo "Starting RDDL Gym Server..."
-( cd /workspace/pyRDDLGym && python prost.py "$DOM" "$INST" "$3" "$4" ) > pyrddl_$1_$2_prost_True_$4.log 2>&1 &
+( cd /workspace/pyRDDLGym && python prost.py "$DOM" "$INST" "$3" TIME) > pyrddl_$1_$2_prost_True_$4.log 2>&1 &
 
 sleep 5
 echo "Starting PROST..."
 ( cd /workspace/prost && ./search-release 813rocks \
-"[PROST -s 1 -se [THTS -act [UCB1] -out [UMC] -backup [PB] -init [Expand -h [IDS]]]]" ) > prost_$1_$2_prost_True_$4.log 2>&1
+"[PROST -se [THTS -T TIME -t $4$ -act [UCB1] -out [UMC] -backup [PB] -init [Expand -h [IDS]]]]" ) > prost_$1_$2_prost_True_$4.log 2>&1
 
-echo "Writing data.json..."
+echo "Writing json output file..."
 cat /workspace/data.json > data_$1_$2_prost_True_$4.json
