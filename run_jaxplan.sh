9a48e20a22476d3fe8366bbe5582996d8ddd7171
Mike Gimelfarb <35513382+mike-gimelfarb@users.noreply.github.com>
2023-11-09 00:48:22 -0500

    Updated run sh files

--- run_jaxplan.sh
+++ run_jaxplan.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-#SBATCH --array=1-10
+#SBATCH --account=rrg-ssanner
+#SBATCH --array=1-1
 #SBATCH --time=01:00:00
-#SBATCH --mem=16G
+#SBATCH --gpus=1
 #SBATCH --cpus-per-task=4
+#SBATCH --mem=8G
 # use case: sbatch run_jaxplan.sh <domain> <online> <time>
 python main.py $1 $SLURM_ARRAY_TASK_ID "jaxplan" $2 True $3
