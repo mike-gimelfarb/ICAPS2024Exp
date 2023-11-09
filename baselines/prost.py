7fdcfb03da508cbae3bce823e3177b69b8fc3ed4
Mike Gimelfarb <35513382+mike-gimelfarb@users.noreply.github.com>
2023-11-09 05:24:09 -0500

    rddlsimagent script updated to disable internal viz

--- baselines/prost.py
+++ baselines/prost.py
@@ -5,6 +5,7 @@ import os
 import sys
 
 from pyRDDLGym.Core.Policies.RDDLSimAgent import RDDLSimAgent
+
 from rddlrepository.Manager.RDDLRepoManager import RDDLRepoManager as RDDLRepoManager
 
 args = sys.argv
@@ -24,7 +25,7 @@ EnvInfo = RDDLRepoManager().get_problem(domain)
 domain_path = EnvInfo.get_domain()
 instance_path = EnvInfo.get_instance(instance)
 
-agent = RDDLSimAgent(domain_path, instance_path, rounds, 999999)
+agent = RDDLSimAgent(domain_path, instance_path, rounds, 999999, disable_viz=True)
 agent.run()
 agent.dump_data(os.path.join(os.environ.get('PROST_OUT'), 
                 f'data_{domain}_{instance}_prost_True_{time}.json'))
