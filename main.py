e74c46caf0b55fe66d246b64330ad8ba66ccf263
Mike Gimelfarb <35513382+mike-gimelfarb@users.noreply.github.com>
2023-11-09 21:40:03 -0500

    Flush prints in main

--- main.py
+++ main.py
@@ -10,14 +10,14 @@ from rddlrepository.Manager.RDDLRepoManager import RDDLRepoManager as RDDLRepoMa
     
 
 def main(domain, instance, method, online, tuning, time):
-    print('receiving args...')
-    print(f'args = {domain}, {instance}, {method}, {online}, {tuning}, {time}')
+    print('receiving args...', flush=True)
+    print(f'args = {domain}, {instance}, {method}, {online}, {tuning}, {time}', flush=True)
         
     # load global config
-    print('loading global config...')
+    print('loading global config...', flush=True)
     ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
     _, global_args = _parse_config_file(os.path.join(ROOT_PATH, 'baselines', 'global.cfg'))
-    print('loading global config complete!')
+    print('loading global config complete!', flush=True)
     
     # path where to log outputs
     outputpath = os.path.join(ROOT_PATH, 'outputs', method,
@@ -31,7 +31,7 @@ def main(domain, instance, method, online, tuning, time):
                   log=True, log_path=outputpath)
     
     # load the config file with planner settings
-    print('loading config...')
+    print('loading config..., flush=True')
     config, args = _parse_config_file(os.path.join(ROOT_PATH, 'configs', f'{domain}.cfg'))
     
     # override default config settings here
@@ -39,10 +39,10 @@ def main(domain, instance, method, online, tuning, time):
     if not online:
         args['rollout_horizon'] = None
     planner_args, plan_args, train_args = _load_config(config, args)
-    print('loading config complete!')
+    print('loading config complete!', flush=True)
     
     # dispatch to policy creation method
-    print('begin policy creation...')
+    print('begin policy creation...', flush=True)
     if method == 'jaxplan':
         from baselines.jaxplan import jax_policy
         policy = jax_policy(env, online, tuning,
@@ -63,19 +63,19 @@ def main(domain, instance, method, online, tuning, time):
     
     else:
         raise Exception(f'Invalid method {method}.')
-    print('policy creation complete!')
+    print('policy creation complete!', flush=True)
     
     # evaluation
-    print('begin policy evaluation...')
+    print('begin policy evaluation...', flush=True)
     result = policy.evaluate(env, verbose=False, episodes=global_args['episodes'])
-    print('policy evaluation complete!')
+    print('policy evaluation complete!', flush=True)
     
     # dump all history to files
-    print('writing logs...')
+    print('writing logs...', flush=True)
     with open(outputpath + '.json', 'w') as fp:
         json.dump(result, fp, indent=4)
     env.close()
-    print('writing logs complete!')
+    print('writing logs complete!', flush=True)
 
 
 if __name__ == '__main__':
