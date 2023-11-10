c9a54550127540433cf83b0f5c0ec3337eb3487f
Mike Gimelfarb <35513382+mike-gimelfarb@users.noreply.github.com>
2023-11-10 00:57:19 -0500

    Fix all run scripts in unix format

--- run_all.sh
+++ run_all.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # use case: source ./run_all.sh <domain> <time>
-sbatch run_gurobiplan.sh $2 True $2
-sbatch run_jaxplan.sh $2 True $2
+#sbatch run_gurobiplan.sh $1 True $2
+sbatch run_jaxplan.sh $1 True $2
 sbatch run_noop.sh $1 True $2
-sbatch run_prost.sh $1 True $2
+#sbatch run_prost.sh $1 True $2
 sbatch run_random.sh $1 True $2
