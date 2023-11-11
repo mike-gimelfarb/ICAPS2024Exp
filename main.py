a241a14f33b4f6fcc5404394bbd5216ad5fdf8ac
Mike Gimelfarb <35513382+mike-gimelfarb@users.noreply.github.com>
2023-11-11 18:43:40 -0500

    Change domain argument to numbered

--- main.py
+++ main.py
@@ -7,12 +7,35 @@ from pyRDDLGym.Core.Jax.JaxRDDLBackpropPlanner import _parse_config_file, _load_
 from pyRDDLGym.Core.Policies.Agents import NoOpAgent, RandomAgent
 
 from rddlrepository.Manager.RDDLRepoManager import RDDLRepoManager as RDDLRepoManager
-    
+
+DOMAIN_LIST = [
+    'AcademicAdvising_MDP_ippc2014',
+    'CooperativeRecon_MDP_ippc2011',
+    'CrossingTraffic_MDP_ippc2011',
+    'Elevators_MDP_ippc2011',
+    'GameOfLife_MDP_ippc2011',
+    'HVAC_ippc2023',
+    'MarsRover_ippc2023',
+    'MountainCar_ippc2023',
+    'Navigation_MDP_ippc2011',
+    'PowerGen_ippc2023',
+    'RaceCar_ippc2023',
+    'Reservoir_ippc2023',
+    'SkillTeaching_MDP_ippc2011',
+    'SysAdmin_MDP_ippc2011',
+    'Tamarisk_MDP_ippc2014',
+    'Traffic_MDP_ippc2011',
+    'TriangleTireworld_MDP_ippc2014',
+    'UAV_ippc2023',
+    'Wildfire_MDP_ippc2014'
+]
+
 
 def main(domain, instance, method, online, tuning, time):
-    print('receiving args...', flush=True)
-    print(f'args = {domain}, {instance}, {method}, {online}, {tuning}, {time}', flush=True)
-        
+    domain_num = domain
+    domain = DOMAIN_LIST[domain_num - 1]
+    print(f'args = {domain}, {instance}, {method}, {online}, {tuning}, {time}\n', flush=True)
+    
     # load global config
     print('loading global config...', flush=True)
     ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
@@ -82,11 +105,12 @@ if __name__ == '__main__':
     args = sys.argv[1:]
     if len(args) != 6:
         print('')
-        print('usage main.py <domain> <instance> <method> <online> <tuning> <time>')
+        print('    usage main.py <domain> <instance> <method> <online> <tuning> <time>')
         print('')
         sys.exit(1)
     domain, instance, method, online, tuning, time = args[:6]
-    domain = str(domain)
+    domain = int(domain)
+    assert domain >= 0
     instance = str(instance)
     online = online in {'True', 'true', True, '1', 1}
     tuning = tuning in {'True', 'true', True, '1', 1}
