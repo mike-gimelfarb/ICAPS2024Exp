a1dfa93a46275ca1033ffdba8d3415d79d6903a7
Mike Gimelfarb <35513382+mike-gimelfarb@users.noreply.github.com>
2023-11-11 01:28:52 -0500

    Remove prost completely for now, fix timing

--- prost/run_docker.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-CONTAINER="$1-$2-$3"
-docker run --name $CONTAINER prost rddlprost $1 $2 $3
-docker cp $CONTAINER:/OUTPUTS/. ../outputs/prost
-docker rm $CONTAINER
--- prost/run_prost.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#SBATCH --array=1-10
-#SBATCH --time=01:00:00
-#SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=4G
-# use case: sbatch run_prost.sh <domain> <online> <time>
-rddlprost $1 $SLURM_ARRAY_TASK_ID $3
