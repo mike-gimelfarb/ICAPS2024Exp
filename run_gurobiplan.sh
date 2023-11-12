a4c5f64b9d4e339872a881452d6f9c6fa827dad4
Mike Gimelfarb <35513382+mike-gimelfarb@users.noreply.github.com>
2023-11-11 23:54:58 -0500

    Update run_gurobiplan.sh

--- run_gurobiplan.sh
+++ run_gurobiplan.sh
@@ -4,4 +4,6 @@
 #SBATCH --cpus-per-task=4
 #SBATCH --mem-per-cpu=4G
 # use case: sbatch run_gurobiplan.sh <domain> <online> <time>
+module load gurobi/10.0.3
+source ~/baselines/bin/activate
 python main.py $1 $SLURM_ARRAY_TASK_ID "gurobiplan" $2 True $3
