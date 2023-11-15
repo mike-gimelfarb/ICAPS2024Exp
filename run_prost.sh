10ff094b493d5de571fa32efaf9074e2115c94a7
Mike Gimelfarb <35513382+mike-gimelfarb@users.noreply.github.com>
2023-11-15 01:55:14 -0500

    Create run_prost.sh

---
+++ run_prost.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#SBATCH --array=1-1
+#SBATCH --time=00:15:00
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=4G
+# use case: sbatch run_prost.sh <domain> <online> <time>
+rddlprost $1 $SLURM_ARRAY_TASK_ID $3
