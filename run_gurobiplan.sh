8c57e17a0fdd9ae2ad42b5eb2b16fc96c8c7d1ed
Mike Gimelfarb <35513382+mike-gimelfarb@users.noreply.github.com>
2023-11-18 19:07:11 -0500

    Updated tuning trials to 5, fixed gurobi.env io

--- run_gurobiplan.sh
+++ run_gurobiplan.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 #SBATCH --array=1-10
-#SBATCH --time=06:00:00
+#SBATCH --time=08:00:00
 #SBATCH --cpus-per-task=4
 #SBATCH --mem-per-cpu=4G
 # use case: sbatch run_gurobiplan.sh <domain> <online> <time>
 module load gurobi/10.0.3
 source ~/baselines/bin/activate
 grb_ts
-echo "Threads ${SLURM_CPUS_ON_NODE:-1}" > gurobi.env
-echo "OutputFlag 0" >> gurobi.env
-echo "NonConvex 2" >> gurobi.env
 python main.py $1 $SLURM_ARRAY_TASK_ID "gurobiplan" $2 True $3
