fbaa1edbec1881f3b1f566ee93fab90e8236c073
Mike Gimelfarb <35513382+mike-gimelfarb@users.noreply.github.com>
2023-11-19 07:51:25 -0500

    Gurobi baseline updated with grid search

--- gurobiplan.py
+++ gurobiplan.py
@@ -9,23 +9,22 @@ from pyRDDLGym.Core.Gurobi.GurobiRDDLPlanner import GurobiOnlineController
 def gurobi_policy(env, online, tuning, args, outputpath, global_args):
     
     # run hyper-parameter tuning
-    params = {'T': args['rollout_horizon']}
+    T = args['rollout_horizon']
     if tuning and online:
         tuning = GurobiParameterTuningReplan(env,
                                              timeout_training=args['train_seconds'],
                                              eval_trials=global_args['trials'],
-                                             num_workers=global_args['batch'],
-                                             gp_iters=global_args['rounds'])        
-        params = tuning.tune(key=int(datetime.now().timestamp()), 
-                             filename=outputpath + '_gp')
+                                             num_workers=global_args['batch'])        
+        T = tuning.tune(key=int(datetime.now().timestamp()),
+                        filename=outputpath + '_gp')
         
     # solve planning problem with new optimal parameters
     model_params = {'OutputFlag': 0, 'NonConvex': 2, 'TimeLimit': args['train_seconds']}
     if online: 
         return GurobiOnlineController(rddl=env.model,
                                       plan=GurobiStraightLinePlan(),
-                                      rollout_horizon=params['T'],
-                                      model_params=model_params, 
+                                      rollout_horizon=T,
+                                      model_params=model_params,
                                       verbose=args['verbose'])
     else:
         return GurobiOfflineController(rddl=env.model,
