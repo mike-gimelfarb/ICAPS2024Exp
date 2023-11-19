26ab34809c64cfd8f0542846f743be6cf80f23e3
Mike Gimelfarb <35513382+mike-gimelfarb@users.noreply.github.com>
2023-11-19 16:45:30 -0500

    Updated run files with new time limits

--- run_jaxplan.sh
+++ run_jaxplan.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --array=1-10
-#SBATCH --time=03:00:00
+#SBATCH --time=04:00:00
 #SBATCH --cpus-per-task=4
 #SBATCH --mem-per-cpu=4G
 # use case: sbatch run_jaxplan.sh <domain> <online> <time>
