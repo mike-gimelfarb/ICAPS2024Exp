e6fcb473455cd1afe14b9eb7c785e3b1ccd85091
Mike Gimelfarb <35513382+mike-gimelfarb@users.noreply.github.com>
2023-11-25 01:25:49 -0500

    Post process can combine all time steps to one file

--- post/process.py
+++ post/process.py
@@ -4,7 +4,7 @@ import os
 import pandas as pd
 
 folderpath = r"C:\Users\mgime\Documents\FINAL RDDL RESULTS"
-
+time_combined = True
     
 def collect_json_to_csv(time_ps):
     
@@ -15,7 +15,9 @@ def collect_json_to_csv(time_ps):
             if file.endswith('.json'):
                 name = os.path.splitext(file)[0]
                 domain, *_, instance, method, online, time = name.split('_')
-                if int(time) == time_ps:
+                if time_combined and method not in ['noop', 'random']:
+                    method = f'{method}_{time}'
+                if method in ['noop', 'random'] or time_combined or int(time) == time_ps:
                     instance = instance.rjust(2, '0')
                     task = f'{domain}_{instance}'
                     with open(os.path.join(path, file)) as contents:
@@ -26,7 +28,10 @@ def collect_json_to_csv(time_ps):
     df = pd.DataFrame(lines, columns=['task', 'method', 'mean', 'std'])
     df = df.sort_values(by=['task', 'method'])
     df = df.pivot(index='task', columns='method')
-    df.to_csv(f'final_raw_{time_ps}.csv')
+    if time_combined:
+        df.to_csv(f'final_raw.csv')
+    else:
+        df.to_csv(f'final_raw_{time_ps}.csv')
     
     # normalize performance
     low = np.maximum(df['mean', 'noop'], df['mean', 'random'])
@@ -35,8 +40,15 @@ def collect_json_to_csv(time_ps):
     for col in df['mean'].columns:
         df['mean', col] = np.clip((df['mean', col] - low) / normalizer, 0., 1.)
         df['std', col] = df['std', col] / normalizer
-    df.to_csv(f'final_normalized_{time_ps}.csv')
+    if time_combined:
+        df.to_csv(f'final_normalized.csv')
+    else:
+        df.to_csv(f'final_normalized_{time_ps}.csv')
     
 if __name__ == '__main__':
-    collect_json_to_csv(1)
+    if time_combined:
+        collect_json_to_csv(None)
+    else:
+        collect_json_to_csv(1)
+        collect_json_to_csv(3)
     
