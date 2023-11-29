bcbe783358ab6f3640a30f46431dbf9df58ffcaf
Mike Gimelfarb <35513382+mike-gimelfarb@users.noreply.github.com>
2023-11-28 19:44:51 -0500

    Save summary json for prost

--- prost_docker/rddlsim.py
+++ prost_docker/rddlsim.py
@@ -1,4 +1,6 @@
 import configparser
+import numpy as np
+import json
 import os
 import sys
 
@@ -30,3 +32,17 @@ agent = RDDLSimAgent(domain_path, instance_path, rounds, 99999)
 agent.run()
 agent.dump_data(os.path.join(os.environ.get('PROST_OUT'), 
                 f'data_{domain}_{instance}_prost_True_{time}.json'))
+
+round_returns = []
+for round_data in agent.logs:
+    round_returns.append(float(round_data[-1]['reward']))
+stats = {
+    'mean': np.mean(round_returns),
+    'median': np.median(round_returns),
+    'min': np.min(round_returns),
+    'max': np.max(round_returns),
+    'std': np.std(round_returns)
+}
+with open(os.path.join(os.environ.get('PROST_OUT'),
+                       f'{domain}_{instance}_prost_True_{time}.json'), "w") as f:
+    json.dump(stats, f)
